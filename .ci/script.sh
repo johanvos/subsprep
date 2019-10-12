386691f7b7145b2f156bc9340e040acda257191b
Johan Vos <johan@lodgon.com>
2019-10-12 09:44:58 +0200

    first try for the script

--- .ci/script.sh
+++ .ci/script.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 echo "hello, ci script"
+wget https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
+unzip graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
+echo "public class HelloWorld { public static void main(String[] args) { System.out.println(\"Hello World\"); } }" > HelloWorld.java
+javac HelloWorld.java
+java -cp build/lib/substrate-0.0.1-SNAPSHOT.jar -Dimagecp=. -Dgraalvm=. -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher 
 # get Graal from https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-darwin-amd64.zip or graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
 # compile HelloWorld
 # java -cp substrate-0.0.1-SNAPSHOT.jar -Dimagecp=path/to/HelloWorld.class -Dgraalvm=path/to/graalinstall -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher 
