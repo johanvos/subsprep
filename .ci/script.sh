67bfa5d2b2103d53ff1c9588e2508b5fd0f6ef31
Johan Vos <johan@lodgon.com>
2019-10-12 10:02:26 +0200

    add path to jar

--- .ci/script.sh
+++ .ci/script.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo "hello, ci script"
+echo "which java: $(which java)"
 wget https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
 unzip graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
 echo "public class HelloWorld { public static void main(String[] args) { System.out.println(\"Hello World\"); } }" > HelloWorld.java
 javac HelloWorld.java
-java -cp build/lib/substrate-0.0.1-SNAPSHOT.jar -Dimagecp=. -Dgraalvm=. -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher 
+java -cp com.gluonhq.substrate/build/libs/com.gluonhq.substrate-0.0.1-SNAPSHOT.jar -Dimagecp=. -Dgraalvm=graalvm-unknown-java11-19.3.0-dev -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher 
 # get Graal from https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-darwin-amd64.zip or graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
 # compile HelloWorld
 # java -cp substrate-0.0.1-SNAPSHOT.jar -Dimagecp=path/to/HelloWorld.class -Dgraalvm=path/to/graalinstall -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher 
