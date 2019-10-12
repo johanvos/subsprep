10ecf76f6da913d1d9c4b406c9d3193f91bf880f
jose.pereda <jose.pereda@gluonhq.com>
2019-10-12 13:11:28 +0200

    update script

--- .ci/script.sh
+++ .ci/script.sh
@@ -2,16 +2,17 @@
 echo "hello, ci script for $1"
 echo "which java: $(which java)"
 sh ./gradlew build
+
+graalvm = "graalvm-unknown-java11-19.3.0-dev"
 if [ "$1" = "linux" ]
 then
   wget https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
   unzip graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
-  graalvm = graalvm-unknown-java11-19.3.0-dev
 elif [ "$1" = "osx" ]
 then
   wget https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-darwin-amd64.zip
   unzip graalvm-unknown-java11-19.3.0-dev-gvm-1-darwin-amd64.zip
-  graalvm = graalvm-unknown-java11-19.3.0-dev/Contents/Home
+  graalvm = "$graalvm/Contents/Home"
 else
   echo "OS $1 not supported"
   exit 1
