b0a365f4d8d7e7171a515fe33a83d093f4e55980
jose.pereda <jose.pereda@gluonhq.com>
2019-10-12 13:05:32 +0200

    Add env to do linux and mac builds

--- .ci/script.sh
+++ .ci/script.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
-echo "hello, ci script"
+echo "hello, ci script for $1"
 echo "which java: $(which java)"
 sh ./gradlew build
-wget https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
-unzip graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
+if [ "$1" = "linux" ]
+then
+  wget https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
+  unzip graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
+  graalvm = graalvm-unknown-java11-19.3.0-dev
+elif [ "$1" = "osx" ]
+then
+  wget https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-darwin-amd64.zip
+  unzip graalvm-unknown-java11-19.3.0-dev-gvm-1-darwin-amd64.zip
+  graalvm = graalvm-unknown-java11-19.3.0-dev/Contents/Home
+else
+  echo "OS $1 not supported"
+  exit 1
+fi
 echo "public class HelloWorld { public static void main(String[] args) { System.out.println(\"Hello World\"); } }" > HelloWorld.java
 javac HelloWorld.java
-java -cp com.gluonhq.substrate/build/libs/com.gluonhq.substrate-0.0.1-SNAPSHOT.jar -Dimagecp=. -Dgraalvm=graalvm-unknown-java11-19.3.0-dev -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher 
-# get Graal from https://download2.gluonhq.com/substrate/graalvm/graalvm-unknown-java11-19.3.0-dev-gvm-1-darwin-amd64.zip or graalvm-unknown-java11-19.3.0-dev-gvm-1-linux-x86_64.zip
-# compile HelloWorld
-# java -cp substrate-0.0.1-SNAPSHOT.jar -Dimagecp=path/to/HelloWorld.class -Dgraalvm=path/to/graalinstall -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher 
+java -cp com.gluonhq.substrate/build/libs/com.gluonhq.substrate-0.0.1-SNAPSHOT.jar -Dimagecp=. -Dgraalvm=$graalvm -Dmainclass=HelloWorld com.gluonhq.substrate.SubstrateDispatcher
